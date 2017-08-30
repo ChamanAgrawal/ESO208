f80831130b16b05ebccbce6bb3327e719ba22eef
Chaman Agrawal <chaman.ag@gmail.com>
2017-08-31 01:44:44 +0530

    Bairstow method WIP

--- prob_2.m
+++ prob_2.m
@@ -1,7 +1,5 @@
 str = input('Give a nonlinear equation in x:','s');
 f= inline(str);
-syms x
-c = coeffs(f,'all')
 fprintf('Enter the method you want to use for :\n1: Muller\n2: Bairstow\n');
 way = input('');
 if way == 1,
@@ -80,13 +78,12 @@ end;
 
 if way == 2,
   syms x
-  c = coeffs(f(x),'All')
-  fprintf('Enter the smaller point\n');
-  x3 = input('');
-  fprintf('Enter the middle point\n');
-  x2 = input('');
-  fprintf('Enter the larger point\n');
-  x1 = input('');
+  c = coeffs(f(x),x)
+  n=size(c,2);
+  fprintf('Enter alpha0\n');
+  alpha0 = input('');
+  fprintf('Enter alpha1\n');
+  alpha1 = input('');
   
   %fprintf('Enter the initial guess for the root:\n');
   %x0 = input('');
@@ -94,7 +91,7 @@ if way == 2,
   miter=input('');
   fprintf('Maximum relative approximate error you want for the method:\n')
   merror = input('');
-  xrange = (x3-5*(x1-x3)):0.1:(x1+5*(x1-x3));
+  xrange = (-10):0.1:(10);
   %xrange = 1:0.1:10
   figure;
   %size(xrange)
@@ -106,44 +103,41 @@ if way == 2,
   plot(xrange,y)
   xlabel('x');
   ylabel('f(x)');
-  y1=f(x1);
-  y2=f(x2);
-  y3=f(x3);
-  a=(((y1-y2)/(x1-x2))-((y2-y3)/(x2-x3)))/(x1-x3);
-  b=((y1-y2)/(x1-x2))+a*(x1-x2);
-  c=y1;
   error=100;
   iter=0;
-  lastx1=111;
+  %lastx1=111;
   flag=0;
-  delta=0;
-  while (flag==0 )|| ((error>merror) && (iter<miter) && (y1~=0))
+  d=zeros(1,n)
+  delta=zeros(1,n-1);
+  while (flag==0 )|| ((error>merror) && (iter<miter) && (y1~=0) && (delta_alpha0~=0))
     
-    delta1 = -2*c/(b+sqrt(b^2-4*a*c));
-    delta2 = -2*c/(b-sqrt(b^2-4*a*c));
-    if abs(delta1)<abs(delta2)
-      delta = delta1;
-    else
-      delta = delta2;
+    d(1,n)=c(1,n);
+    d(1,n-1)=c(1,n-1)+alpha1*d(1,n);
+    for i=n-2:1
+      d(1,i) = c(1,i)+alpha1*d(1,i+1)+alpha0*d(1,i+2);
     end;
-    x3=x2;
-    x2=x1;
-    x1= x1+delta;
+    delta(n-1) = d(n);
+    delta(n-2) = d(n-1)+alpha1*delta(n-1);
+    for i=n-3:1
+      delta(i) = d(i+1)+alpha1*delta(i+1)+alpha0*d(i+2);
+    end;
+    delta_alpha0 = (delta(1)*d(2)-delta(2)*d(1))/(delta(2)*delta(2)-delta(1)*delta(3));
+    delta_alpha1 = (delta(3)*d(1)-delta(2)*d(2))/(delta(2)*delta(2)-delta(1)*delta(3));
+    alpha0=alpha0+delta_alpha0;
+    alpha1=alpha1+delta_alpha1;
     if flag==1
       error=abs((lastx1-x1)*100/x1);
     end;
-    lastx1=x1;
-    y1=f(x1);
-    y2=f(x2);
-    y3=f(x3);
-    a=(((y1-y2)/(x1-x2))-((y2-y3)/(x2-x3)))/(x1-x3);
-    b=((y1-y2)/(x1-x2))+a*(x1-x2);
-    c=y1;
+    %lastx1=x1;
+    
     iter= iter + 1;
     flag=1;
     
   end;  
-  x1
+  root1 = 0.5*(alpha1+sqrt(alpha1*alpha1+4*alpha0));
+  root2 = 0.5*(alpha1-sqrt(alpha1*alpha1+4*alpha0));
+  root1
+  root2
   %iterations = 2:1:iter;
   %error =error(1,2:iter);
   %figure;
@@ -154,3 +148,4 @@ if way == 2,
 end;
 
 clear
+
