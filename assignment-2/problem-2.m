9c2ba3b5ae855f2384e7d6f30734e8939ba1c2a4
Chaman Agrawal <chaman.ag@gmail.com>
2017-09-13 00:17:42 +0530

    Assignment complete (no. of iterations to be redured)

--- assignment-2/problem-2.m
+++ assignment-2/problem-2.m
@@ -25,7 +25,9 @@ if method == 1
   z(1,1) = 1;
   zmod = 1;
   iter = 1;
+  value(iter,1) =100;
   error =100;
+  value(iter,2) = 1;
   while (iter<miter && error>merror)
     y = A*z;
     ymod=0;
@@ -33,12 +35,27 @@ if method == 1
       ymod = ymod + y(i,1)^2;
     end;
     ymod = sqrt(ymod);
-    error = abs((ymod - zmod)/ymod)*100;
+    y = y./ymod;
+    curerror=0;
+    cureigenvec = A*y;
+    cureigenval = 0;
+    for i=1:1:size
+      cureigenval = cureigenval + cureigenvec(i,1)^2 ;
+    end;
+    cureigenval = sqrt(cureigenval);
+    for i=1:1:size
+        curerror = max (curerror,(abs((z(i,1)-y(i,1))/y(i,1))*100));
+    end;
+    value(iter,2) = cureigenval;
+    value(iter,1) = curerror;
+    error = curerror;
     zmod = ymod;
-    z = y./ymod;
+    z = y;
     iter = iter+1;
   end;
   z
+  fprintf("  (Error,Eigenvalue) is \n");
+  value
   y=A*z;
   ymod = 0;
   for i=1:size
@@ -47,12 +64,16 @@ if method == 1
   eigenvalue = sqrt(ymod)
   filename = "output2.txt";
   outf = fopen (filename, "w");
-  fdisp(outf , "Power Method\nEigenvalue\n");
+  fdisp(outf, "")
+  fdisp(outf , "Power Method\n\n");
+  fdisp(outf, "(Error,Eigenvalue) is\n");
+  fdisp(outf, value);
+  fdisp(outf, "\nEigenvalue\n");
   fdisp(outf ,eigenvalue);
   fdisp(outf,"\nEigenvector\n");
   fdisp(outf,z);
   fdisp(outf,"\nIterations\n");
-  fdisp(outf,iter);
+  fdisp(outf,iter-1);
   fclose(outf);
 end;
 
@@ -80,6 +101,8 @@ if method == 2
   zmod = 1;
   iter = 1;
   error =100;
+  value(iter,1) =100;
+  value(iter,2) =1;
   while (iter<miter && error>merror)
     y = B*z;
     ymod=0;
@@ -87,12 +110,27 @@ if method == 2
       ymod = ymod + y(i,1)^2;
     end;
     ymod = sqrt(ymod);
-    error = abs((ymod - zmod)/ymod)*100;
+    y = y./ymod;
+    curerror=0;
+    cureigenvec = A*y;
+    cureigenval = 0;
+    for i=1:1:size
+      cureigenval = cureigenval + cureigenvec(i,1)^2 ;
+    end;
+    cureigenval = sqrt(cureigenval);
+    for i=1:1:size
+        curerror = max (curerror,(abs((z(i,1)-y(i,1))/y(i,1))*100));
+    end;
+    value(iter,2) = cureigenval;
+    value(iter,1) = curerror;
+    error = curerror;
     zmod = ymod;
-    z = y./ymod;
+    z = y;
     iter = iter+1;
   end;
   z
+  fprintf("  (Error,Eigenvalue) is \n");
+  value
   y=A*z;
   ymod = 0;
   for i=1:size
@@ -101,12 +139,15 @@ if method == 2
   eigenvalue = sqrt(ymod)
   filename = "output2.txt";
   outf = fopen (filename, "w");
-  fdisp(outf , "Inverse Power Method\nEigenvalue\n");
+  fdisp(outf , "Inverse Power Method\n");
+  fdisp(outf, "(Error,Eigenvalue) is\n");
+  fdisp(outf, value);
+  fdisp(outf, "\nEigenvalue\n");
   fdisp(outf ,eigenvalue);
   fdisp(outf,"\nEigenvector\n");
   fdisp(outf,z);
   fdisp(outf,"\nIterations\n");
-  fdisp(outf,iter);
+  fdisp(outf,iter-1);
   fclose(outf);
 end;
 
@@ -135,7 +176,9 @@ if method == 3
   z(1,1) = 1;
   zmod = 1;
   iter = 1;
-  error =100;
+  value(iter,1) =100;
+  value(iter,2) = 1;
+  error = 100;
   while (iter<miter && error>merror)
     y = B*z;
     ymod=0;
@@ -143,12 +186,27 @@ if method == 3
       ymod = ymod + y(i,1)^2;
     end;
     ymod = sqrt(ymod);
-    error = abs((ymod - zmod)/ymod)*100;
+    y = y./ymod;
+    curerror=0;
+    for i=1:1:size
+        curerror = max (curerror,(abs((z(i,1)-y(i,1))/y(i,1))*100));
+    end;
+    cureigenval = 0;
+    cureigenvec = A*y;
+    for l=1:1:size
+      cureigenval = cureigenval + cureigenvec(l,1)^2;
+    end;
+    cureigenval = sqrt(cureigenval);
+    value(iter,2) = cureigenval;
+    value(iter,1) = curerror;
+    error = curerror;
     zmod = ymod;
-    z = y./ymod;
+    z = y;
     iter = iter+1;
   end;
   z
+  fprintf("  (Error,Eigenvalue) is \n");
+  value
   y=A*z;
   ymod = 0;
   for i=1:size
@@ -157,13 +215,93 @@ if method == 3
   eigenvalue = sqrt(ymod)
   filename = "output2.txt";
   outf = fopen (filename, "w");
-  fdisp(outf , "Inverse Power Method with shift\nEigenvalue\n");
+  fdisp(outf , "Inverse Power Method with shift\n");
+  fdisp(outf, "(Error,Eigenvalue) is\n");
+  fdisp(outf, value);
+  fdisp(outf, "\nEigenvalue\n");
   fdisp(outf ,eigenvalue);
   fdisp(outf,"\nEigenvector\n");
   fdisp(outf,z);
   fdisp(outf,"\nIterations\n");
-  fdisp(outf,iter);
+  fdisp(outf,iter-1);
   fclose(outf);
 end;
 
+if method == 4
+  fprintf("Give your input in a file named 'input2.txt'\n");
+  fprintf("FORMAT: Size of matrix \n matrix \n Maximum iterations \n Maximum approximate relative error \n");
+  fprintf("\nPress ENTER to continue\n\n");
+  pause;
+  fid = fopen("input2.txt");
+  line =fgetl(fid);
+  size = sscanf(line, '%f ');
+  A = zeros(size,size);
+  for i=1:1:size
+    line = fgetl(fid);
+    A(i,1:1:size) = sscanf(line, '%f ');
+  end;
+  line = fgetl(fid);
+  miter = sscanf(line, '%f ');
+  line = fgetl(fid);
+  merror = sscanf(line, '%f ');
+  iter = 1;
+  value(iter,1) =100;
+  value(iter,2) = 1;
+  error = 100;
+  while (iter<miter && error>merror)
+    Q = zeros(size,size);
+    for i=1:1:size
+      if i==1
+        ymod=0;
+        for j=1:size
+          ymod = ymod + A(j,1)^2;
+        end;
+        ymod = sqrt(ymod);
+        Q(:,1) = A(:,1)./ymod;
+      else
+        Q(:,i) = A(:,i);
+        for k = k=1:i-1
+          Q(:,i) = Q(:,i) - ((A(:,i)')*Q(:,k)).*Q(:,k);
+        end;
+        ymod=0;
+        for j=1:1:size
+          ymod = ymod + Q(j,i)^2;
+        end;
+        ymod = sqrt(ymod);
+        Q(:,i) = Q(:,i)./ymod;
+      end;
+    end;
+    R = Q'*A;
+    B = R*Q;
+    curerror=0;
+    for i=1:1:size
+        curerror = max (curerror,(abs((A(i,i)-B(i,i))/B(i,i))*100));
+    end;
+    A = B;
+    error = curerror;
+    value(iter,1) = iter;
+    value(iter , 2) = error;
+    for i=3:1:size+2
+      value(iter, i) = A(i-2,i-2);
+    end;
+    iter = iter+1;
+  end;
+  for i=1:size
+    eigenvalue(1,i) = A(i,i);
+  end;
+  fprintf("       (Iterations,Error,Eigenvalues) is \n");
+  value
+  eigenvalue
+  filename = "output2.txt";
+  outf = fopen (filename, "w");
+  fdisp(outf , "QR Method\n");
+  fdisp(outf, "(Error,Eigenvalue) is\n");
+  fdisp(outf, value);
+  fdisp(outf, "\nEigenvalue\n");
+  fdisp(outf ,eigenvalue);
+  fdisp(outf,"\nIterations\n");
+  fdisp(outf,iter-1);
+  fclose(outf);
+
+end;
 fprintf("Output is in output2.txt\n");
